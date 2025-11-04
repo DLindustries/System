da148cce957c0402c61219d3584326263f9f779d
DLindustries <duncanl.studio@gmail.com>
2025-11-04 17:04:25 +0000

    m

---
+++ src/main/java/dlindustries/vigillant/system/module/Category.java
@@ -0,0 +1,20 @@
+package dlindustries.vigillant.system.module;
+
+import dlindustries.vigillant.system.utils.EncryptedString;
+
+public enum Category {
+	sword(EncryptedString.of("Sword")),
+	CRYSTAL(EncryptedString.of("Crystal")),
+	pot(EncryptedString.of("Potions")),
+	mace(EncryptedString.of("Mace")),
+	optimizer(EncryptedString.of("Optimizer")),
+	RENDER(EncryptedString.of("Render")),
+	Donut(EncryptedString.of("Donut SMP")),
+
+	CLIENT(EncryptedString.of("Client"));
+	public final CharSequence name;
+
+	Category(CharSequence name) {
+		this.name = name;
+	}
+}
